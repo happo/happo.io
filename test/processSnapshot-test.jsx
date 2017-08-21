f1a543f244f1addb2d3d54bb1f1697c9662f72ac
Henric Trotzig <henric.trotzig@brigade.com>
2017-08-21 16:15:12 +0200

    Restructure processSnapshot test
    So that it's clearer what we're testing. I've left todos for two
    alternative css solutions that I aim to test as well.

--- test/processSnapshot-test.jsx
+++ test/processSnapshot-test.jsx
@@ -4,26 +4,34 @@ import styled from 'styled-components';
 import { snap } from '../src';
 import processSnap from '../src/processSnap';
 
-const Title = styled.h1`
-  transform: translateY(-10%);
-  font-size: 1.5em;
-  text-align: center;
-  color: red;
-`;
+describe('with styled-components', () => {
+  it('provides a serializable payload', () => {
+    const Title = styled.h1`
+      transform: translateY(-10%);
+      color: red;
+    `;
 
-class Foo extends PureComponent {
-  render() {
-    return (
-      <div className="foo">
-        <Title>Test</Title>
-      </div>
-    );
-  }
-}
+    class Foo extends PureComponent {
+      render() {
+        return (
+          <div className="foo">
+            <Title>Test</Title>
+          </div>
+        );
+      }
+    }
 
-it('provides a serializable payload', () => {
-  const snapshot = snap('foo', <Foo />);
-  const payload = processSnap(snapshot);
-  expect(payload.css).toMatch(/-webkit-transform:.*color:red;/);
-  expect(payload.html).toMatch(/<div.*><h1.*>Test<\/h1>/);
+    const snapshot = snap('foo', <Foo />);
+    const payload = processSnap(snapshot);
+    expect(payload.css).toMatch(/-webkit-transform:.*color:red;/);
+    expect(payload.html).toMatch(/<div.*><h1.*>Test<\/h1>/);
+  });
+});
+
+describe('with aphrodite', () => {
+  // todo
+});
+
+describe('with css-loader through webpack', () => {
+  // todo
 });
