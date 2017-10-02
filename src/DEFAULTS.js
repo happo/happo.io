eefe1cc93758bbf15941e7e513e923205bdc5e97
Henric Trotzig <henric.trotzig@brigade.com>
2017-10-02 21:18:11 +0200

    Fix default for endpoint config
    I forgot to include this change in an earlier commit.

--- src/DEFAULTS.js
+++ src/DEFAULTS.js
@@ -1,7 +1,6 @@
 import reactDOMRunner from './reactDOMRunner';
 
-export const viewerEndpoint = 'https://happo.now.sh';
-export const snapEndpoint = 'https://happo-snap.now.sh';
+export const endpoint = 'https://happo.now.sh';
 export const include = '**/@(*-snaps|snaps).@(js|jsx)';
 export const stylesheets = [];
 export const targets = {};
