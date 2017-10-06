1d2f7c9f8b8c4220efb8bd95658985749e61b198
Henric Trotzig <henric.trotzig@brigade.com>
2017-10-06 13:31:16 +0200

    Lazy-load nodegit
    It has a tendency to fail on some systems, and it's not a required thing
    (won't be used if you run `happo run sha`).

--- src/findRepository.js
+++ src/findRepository.js
@@ -1,11 +1,13 @@
-import Git from 'nodegit';
-
 import path from 'path';
 
 export default async function findRepository(directory = process.cwd()) {
   if (directory === '/') {
     throw new Error('No git repository root found');
   }
+  // we lazy-load nodegit because it has a tendency to fail on some systems, and
+  // there are ways to work around that (passing in a sha to `happo run`);
+  const Git = require('nodegit');
+
   try {
     return await Git.Repository.open(directory);
   } catch (e) {
