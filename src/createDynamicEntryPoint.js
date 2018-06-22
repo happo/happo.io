9ae3a3570f67ed13d05fb58e500e7aec658e4c65
Henric Trotzig <henric.trotzig@gmail.com>
2018-06-22 22:00:04 +0200

    Use async/await in createDynamicEntryPoint.js
    Using Promise.then makes things harder to read.

--- src/createDynamicEntryPoint.js
+++ src/createDynamicEntryPoint.js
@@ -6,43 +6,42 @@ import requireRelative from 'require-relative';
 
 import findTestFiles from './findTestFiles';
 
-export default function createDynamicEntryPoint({ setupScript, include, only, type }) {
-  return findTestFiles(include).then((files) => {
-    const filePartOfOnly = only ? only.split('#')[0] : undefined;
-    const fileStrings = files
-      .filter((f) => (filePartOfOnly ? f.includes(filePartOfOnly) : true))
-      .map((file) => `window.snaps['${file}'] = require('${path.join(process.cwd(), file)}');`);
-    const strings = [
-      setupScript ? `require('${setupScript}');` : '',
-      'window.snaps = {};',
-      `window.happoFlags = { only: ${JSON.stringify(only)} }`,
-    ].concat(fileStrings);
-    if (type === 'react') {
-      const pathToReactDom = requireRelative.resolve('react-dom', process.cwd());
-      strings.push(
-        `
-        const ReactDOM = require('${pathToReactDom}');
-        window.happoRender = (component, { rootElement }) =>
-          ReactDOM.render(component, rootElement);
-      `.trim(),
-      );
-    } else {
-      strings.push(
-        `
-        window.happoRender = (html, { rootElement }) => {
-          rootElement.innerHTML = html;
-          return rootElement;
-        };
-      `.trim(),
-      );
-    }
-    strings.push('window.onBundleReady();');
-    const entryFile = path.join(
-      os.tmpdir(),
-      `happo-entry-${type}-${Buffer.from(process.cwd()).toString('base64')}.js`,
+export default async function createDynamicEntryPoint({ setupScript, include, only, type }) {
+  const files = await findTestFiles(include);
+  const filePartOfOnly = only ? only.split('#')[0] : undefined;
+  const fileStrings = files
+    .filter((f) => (filePartOfOnly ? f.includes(filePartOfOnly) : true))
+    .map((file) => `window.snaps['${file}'] = require('${path.join(process.cwd(), file)}');`);
+  const strings = [
+    setupScript ? `require('${setupScript}');` : '',
+    'window.snaps = {};',
+    `window.happoFlags = { only: ${JSON.stringify(only)} }`,
+  ].concat(fileStrings);
+  if (type === 'react') {
+    const pathToReactDom = requireRelative.resolve('react-dom', process.cwd());
+    strings.push(
+      `
+      const ReactDOM = require('${pathToReactDom}');
+      window.happoRender = (component, { rootElement }) =>
+        ReactDOM.render(component, rootElement);
+    `.trim(),
     );
+  } else {
+    strings.push(
+      `
+      window.happoRender = (html, { rootElement }) => {
+        rootElement.innerHTML = html;
+        return rootElement;
+      };
+    `.trim(),
+    );
+  }
+  strings.push('window.onBundleReady();');
+  const entryFile = path.join(
+    os.tmpdir(),
+    `happo-entry-${type}-${Buffer.from(process.cwd()).toString('base64')}.js`,
+  );
 
-    fs.writeFileSync(entryFile, strings.join('\n'));
-    return { entryFile, numberOfFilesProcessed: fileStrings.length };
-  });
+  fs.writeFileSync(entryFile, strings.join('\n'));
+  return { entryFile, numberOfFilesProcessed: fileStrings.length };
 }
