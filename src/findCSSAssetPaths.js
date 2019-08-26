56821be8d4a5186f6d0a2c39425b2420714a0479
Henric Trotzig <henric.trotzig@gmail.com>
2019-08-26 09:37:24 +0200

    Add some jsdoc to explain findCSSAssetPaths
    This is a follow-up to https://github.com/happo/happo.io/pull/102 where
    some confusion was brought up related to params and return values of
    findCSSAssetPaths.

--- src/findCSSAssetPaths.js
+++ src/findCSSAssetPaths.js
@@ -4,6 +4,15 @@ import matchAll from 'string.prototype.matchall';
 
 const URL_PATTERN = /url\(['"]?(\/?[^)"']+)['"]?\)/g;
 
+/**
+ * @param {string} css - The css code, e.g. ".foo { background: url(/foo.png); }"
+ * @param {string} [source] - An optional path to the css file where the css is
+ *   coming from. This is used to make the resulting `resolvePath` relative to
+ *   the css file.
+ * @return {object} an object containing an `assetPath` and a `resolvePath`. The
+ *   assetPath is the path as it's described in the css. The resolvePath is the
+ *   path where we should attempt to locate the file in the filesystem.
+ */
 export default function findCSSAssetPaths({ css, source }) {
   const paths = Array.from(matchAll(css, URL_PATTERN))
     .map((match) => match[1])
