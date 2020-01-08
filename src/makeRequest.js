c86f5d4e04fa5ddeb1c85de0164d47e44c4851dd
Henric Trotzig <henric.trotzig@gmail.com>
2020-01-08 10:55:37 +0100

    Switch to async-retry for makeRequest
    The retry logic here was all custom. Now that we have a dependency on
    async-retry, we might as well remove the custom retry code.
    
    The custom logic handled exponential backoff and jitter. I believe
    async-retry handles both of these: backoff is handled by the `retry`
    module (a dependency of async-retry) per default. And jitter is applied
    by async-retry per default (it is off by default in `retry`). See
    https://github.com/tim-kos/node-retry#retrytimeoutsoptions and
    https://www.npmjs.com/package/async-retry#api
    
    Apart from slightly different retry timings, this change will update the
    console warning when a retry is made. Before, the next retry time would
    be logged. We don't have that information now, so we have to adjust the
    log a little.

--- src/makeRequest.js
+++ src/makeRequest.js
@@ -1,50 +1,35 @@
+import asyncRetry from 'async-retry';
 import jwt from 'jsonwebtoken';
 import request from 'request-promise-native';
-
-function randomIntInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
 export default async function makeRequest(
   requestAttributes,
   { apiKey, apiSecret, maxTries = 0 },
-  tryNumber = 1,
 ) {
-  const signed = jwt.sign({ key: apiKey }, apiSecret, { header: { kid: apiKey } });
-  try {
-    const result = await request(
-      Object.assign(
-        {
-          gzip: true,
-          auth: {
-            bearer: signed,
+  return asyncRetry(
+    async () => {
+      const signed = jwt.sign({ key: apiKey }, apiSecret, {
+        header: { kid: apiKey },
+      });
+      const result = await request(
+        Object.assign(
+          {
+            gzip: true,
+            auth: {
+              bearer: signed,
+            },
           },
-        },
-        requestAttributes,
-      ),
-    );
-    return result;
-  } catch (e) {
-    if (tryNumber >= maxTries) {
-      throw e;
-    }
-
-    const { method, url } = requestAttributes;
-
-    // Exponential backoff with full jitter.
-    // https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
-    const nextRetryIn = randomIntInRange(1000, 1000 * 2 ** tryNumber);
-    console.warn(`Failed ${method} ${url}. Retrying after ${(nextRetryIn / 1000).toFixed(1)}s...`);
-
-    await new Promise((resolve) => setTimeout(resolve, nextRetryIn));
-    return makeRequest(
-      requestAttributes,
-      {
-        apiKey,
-        apiSecret,
-        maxTries,
+          requestAttributes,
+        ),
+      );
+      return result;
+    },
+    {
+      retries: maxTries,
+      onRetry: () => {
+        const { method, url } = requestAttributes;
+        console.warn(`Failed ${method} ${url}. Retrying...`);
       },
-      tryNumber + 1,
-    );
-  }
+    },
+  );
 }
