e4cdb752289bc158c2593081d01012e6b7e59534
Joe Lencioni <joe.lencioni@airbnb.com>
2020-11-24 10:22:39 -0600

    Use jest matchers in Logger-test
    This cleans up the tests a bit and should probably give us some nicer
    error messages when they fail.

--- test/Logger-test.js
+++ test/Logger-test.js
@@ -21,53 +21,49 @@ it('works without injected printers', () => {
 
 it('does not print to stdout on errors', () => {
   subject().error(new Error('foo'));
-  expect(print.mock.calls.length).toBe(0);
+  expect(print).toHaveBeenCalledTimes(0);
 });
 
 it('logs errors with stacks', () => {
   const error = new Error('damn');
   error.stack = 'foobar';
   subject().error(error);
-  // We have to use `toMatch` here because the string is wrapped with color
+  // We use `stringContaining` here because the string is wrapped with color
   // instruction characters
-  expect(stderrPrint.mock.calls[0][0]).toMatch(/foobar/);
+  expect(stderrPrint).toHaveBeenNthCalledWith(1, expect.stringContaining('foobar'));
 });
 
 it('logs errors without stacks', () => {
   const error = new Error('damn');
   delete error.stack;
   subject().error(error);
-  // We have to use `toMatch` here because the string is wrapped with color
+  // We use `stringContaining` here because the string is wrapped with color
   // instruction characters
-  expect(stderrPrint.mock.calls[0][0]).toMatch(/damn/);
+  expect(stderrPrint).toHaveBeenNthCalledWith(1, expect.stringContaining('damn'));
 });
 
 it('logs durations with start() and success()', () => {
   const logger = subject();
 
   logger.start('Pizza');
-  let printed = print.mock.calls.map(([str]) => str).join('');
-  expect(printed).toMatch(/Pizza/);
+  expect(print).toHaveBeenNthCalledWith(1, expect.stringContaining('Pizza'));
+  print.mockReset();
 
   logger.success('Yum');
-  printed = print.mock.calls.map(([str]) => str).join('');
-  expect(printed).toMatch(/Pizza/);
-  expect(printed).toMatch(/Yum/);
-  expect(printed).toMatch(/\(\d+\.\d+ms\)/);
+  expect(print).toHaveBeenNthCalledWith(2, expect.stringContaining('Yum'));
+  expect(print).toHaveBeenNthCalledWith(3, expect.stringMatching(/\(\d+\.\d+ms\)/));
 });
 
 it('logs durations with start() and fail()', () => {
   const logger = subject();
 
   logger.start('Pizza');
-  let printed = print.mock.calls.map(([str]) => str).join('');
-  expect(printed).toMatch(/Pizza/);
+  expect(print).toHaveBeenNthCalledWith(1, expect.stringContaining('Pizza'));
+  print.mockReset();
 
   logger.fail('Yuck');
-  printed = print.mock.calls.map(([str]) => str).join('');
-  expect(printed).toMatch(/Pizza/);
-  expect(printed).toMatch(/Yuck/);
-  expect(printed).toMatch(/\(\d+\.\d+ms\)/);
+  expect(print).toHaveBeenNthCalledWith(2, expect.stringContaining('Yuck'));
+  expect(print).toHaveBeenNthCalledWith(3, expect.stringMatching(/\(\d+\.\d+ms\)/));
 });
 
 describe('logTag()', () => {
