happo/happo.io

commit 48b1d5dd1a570a4c4a5d32c908e40db242be0d12

Upload assets for prerender runs separately
To reduce traffic towards s3 and avoid having to be throttled, we can
attempt to reuse asset packages between runs. This is already done for
static runs (e.g. storybook) but it's a little trickier to reuse things
between prerendered runs because assets might differ based on the size
of the viewport. Still, we can use a cache of known asset packages and
avoid the upload if the package is known.

I plan on pairing this change with an update to happo servers as well,
where we avoid the s3 upload if we come across a package that we've seen
and uploaded before. The cache used here is simply local to the cli
invocation, but a cache on happo servers can live longer than that.

src/domRunner.js
@@ -17,16 +17,43 @@ import makeRequest from './makeRequest';
 import prepareAssetsPackage from './prepareAssetsPackage';
 import processSnapsInBundle from './processSnapsInBundle';
 
+const knownAssetPackagePaths = {};
+
 const { VERBOSE = 'false' } = process.env;
+
+async function uploadAssets({ apiKey, apiSecret, endpoint, hash, buffer }) {
+  const assetsRes = await makeRequest(
+    {
+      url: `${endpoint}/api/snap-requests/assets/${hash}`,
+      method: 'POST',
+      json: true,
+      formData: {
+        payload: {
+          options: {
+            filename: 'payload.zip',
+            contentType: 'application/zip',
+          },
+          value: buffer,
+        },
+      },
+    },
+    { apiKey, apiSecret, maxTries: 2 },
+  );
+  return assetsRes.path;
+}
 
 function logTargetResults({ name, globalCSS, snapPayloads, project }) {
   const cssPath = path.join(os.tmpdir(), `happo-verbose-${name}.css`);
   const snippetsPath = path.join(os.tmpdir(), `happo-snippets-${name}.json`);
   fs.writeFileSync(cssPath, JSON.stringify(globalCSS));
   fs.writeFileSync(snippetsPath, JSON.stringify(snapPayloads));
-  console.log(`${logTag(project)}Recorded CSS for target "${name}" can be found in ${cssPath}`);
   console.log(
-    `${logTag(project)}Recorded HTML snippets for target "${name}" can be found in ${snippetsPath}`,
+    `${logTag(project)}Recorded CSS for target "${name}" can be found in ${cssPath}`,
+  );
+  console.log(
+    `${logTag(
+      project,
+    )}Recorded HTML snippets for target "${name}" can be found in ${snippetsPath}`,
   );
 }
 
@@ -59,15 +86,14 @@ function resolveDomProvider({ plugins, jsdomOptions }) {
 async function executeTargetWithPrerender({
   name,
   targets,
-  css,
+  globalCSS,
   snapPayloads,
-  publicFolders,
-  cssBlocks,
   apiKey,
   apiSecret,
   endpoint,
   isAsync,
   project,
+  assetsPackage,
 }) {
   if (!snapPayloads.length) {
     console.warn(`${logTag(project)}No examples found for target ${name}, skipping`);
@@ -81,20 +107,9 @@ async function executeTargetWithPrerender({
     throw new MultipleErrors(errors);
   }
 
-  const globalCSS = cssBlocks.concat([{ css }]);
-
   if (VERBOSE === 'true') {
     logTargetResults({ name, globalCSS, snapPayloads, project });
   }
-
-  const assetsPackage = await prepareAssetsPackage({
-    globalCSS,
-    snapPayloads,
-    publicFolders,
-  });
-  snapPayloads.forEach((item) => {
-    delete item.assetPaths;
-  });
 
   const result = await targets[name].execute({
     asyncResults: isAsync,
@@ -120,24 +135,14 @@ async function uploadStaticPackage({
     tmpdir,
     publicFolders,
   });
-  const assetsRes = await makeRequest(
-    {
-      url: `${endpoint}/api/snap-requests/assets/${hash}`,
-      method: 'POST',
-      json: true,
-      formData: {
-        payload: {
-          options: {
-            filename: 'payload.zip',
-            contentType: 'application/zip',
-          },
-          value: buffer,
-        },
-      },
-    },
-    { apiKey, apiSecret, maxTries: 2 },
-  );
-  return assetsRes.path;
+  const assetsPath = await uploadAssets({
+    apiKey,
+    apiSecret,
+    endpoint,
+    hash,
+    buffer,
+  });
+  return assetsPath;
 }
 
 async function generateScreenshots(
@@ -180,7 +185,11 @@ async function generateScreenshots(
   const targetNames = Object.keys(targets);
   const tl = targetNames.length;
   const DomProvider = resolveDomProvider({ plugins, jsdomOptions });
-  logger.info(`${logTag(project)}Generating screenshots in ${tl} target${tl > 1 ? 's' : ''}...`);
+  logger.info(
+    `${logTag(project)}Generating screenshots in ${tl} target${
+      tl > 1 ? 's' : ''
+    }...`,
+  );
   try {
     const staticPackage = prerender
       ? undefined
@@ -198,24 +207,46 @@ async function generateScreenshots(
       for (const name of targetNames) {
         // These tasks are CPU-bound, and we need to be careful about how much
         // memory we are using at one time, so we want to run them serially.
-        // eslint-disable-next-line no-await-in-loop
+        /* eslint-disable no-await-in-loop */
         const { css, snapPayloads } = await processSnapsInBundle(bundleFile, {
           targetName: name,
           publicFolders,
           viewport: targets[name].viewport,
           DomProvider,
         });
+        const globalCSS = cssBlocks.concat([{ css }]);
+
+        const { buffer, hash } = await prepareAssetsPackage({
+          globalCSS,
+          snapPayloads,
+          publicFolders,
+        });
+
+        const assetsPackage =
+          knownAssetPackagePaths[hash] ||
+          (await uploadAssets({
+            endpoint,
+            apiKey,
+            apiSecret,
+            hash,
+            buffer,
+          }));
+        /* eslint-enable no-await-in-loop */
+
+        snapPayloads.forEach((item) => {
+          delete item.assetPaths;
+        });
+
         prerenderPromises.push(
           (async () => {
             const startTime = performance.now();
             const result = await executeTargetWithPrerender({
               name,
-              css,
+              globalCSS,
               snapPayloads,
               targets,
-              publicFolders,
+              assetsPackage,
               viewport: targets[name].viewport,
-              cssBlocks,
               apiKey,
               apiSecret,
               endpoint,

src/prepareAssetsPackage.js
@@ -1,4 +1,5 @@
 import { Writable } from 'stream';
+import crypto from 'crypto';
 import fs from 'fs';
 import path from 'path';
 
@@ -24,7 +25,8 @@ function makePackage({ paths, publicFolders }) {
     };
     stream.on('finish', () => {
       const buffer = Buffer.from(data);
-      resolve(buffer);
+      const hash = crypto.createHash('md5').update(buffer).digest('hex');
+      resolve({ buffer, hash });
     });
     archive.pipe(stream);
 
@@ -72,7 +74,11 @@ function makePackage({ paths, publicFolders }) {
   });
 }
 
-export default function prepareAssetsPackage({ globalCSS, snapPayloads, publicFolders }) {
+export default function prepareAssetsPackage({
+  globalCSS,
+  snapPayloads,
+  publicFolders,
+}) {
   const paths = {};
   globalCSS.forEach(({ css, source }) => {
     findCSSAssetPaths({ css, source }).forEach(({ assetPath, resolvePath }) => {

test/prepareAssetsPackage-test.js
@@ -35,7 +35,8 @@ beforeEach(() => {
 
 it('creates a package when there are assets', async () => {
   const result = await subject();
-  expect(result).not.toBe(undefined);
+  expect(result.buffer).not.toBe(undefined);
+  expect(result.hash).not.toBe(undefined);
 });
 
 it('creates consistent results', async () => {
@@ -50,11 +51,12 @@ it('creates consistent results', async () => {
 it('does not fail when files are missing', async () => {
   const result = await subject();
   publicFolders = [path.join(__dirname, 'foobar')];
-  expect(result).not.toBe(undefined);
+  expect(result.buffer).not.toBe(undefined);
+  expect(result.hash).not.toBe(undefined);
 });
 
 it('picks out the right files', async () => {
-  const buffer = await subject();
+  const { buffer } = await subject();
   const zip = new AdmZip(buffer);
   expect(zip.getEntries().map(({ entryName }) => entryName)).toEqual([
     '1x1.jpg', // this is in the root because the css is always served from the root on happo workers

commit 68bdcce4435a0c541a5cba1903c2d85417d15e52

Fix error handling in domRunner.js
I broke this in 48b1d5d when I moved some stuff around.

src/domRunner.js
@@ -99,14 +99,6 @@ async function executeTargetWithPrerender({
     console.warn(`${logTag(project)}No examples found for target ${name}, skipping`);
     return [];
   }
-  const errors = snapPayloads.filter((p) => p.isError);
-  if (errors.length === 1) {
-    throw errors[0];
-  }
-  if (errors.length > 1) {
-    throw new MultipleErrors(errors);
-  }
-
   if (VERBOSE === 'true') {
     logTargetResults({ name, globalCSS, snapPayloads, project });
   }
@@ -214,6 +206,15 @@ async function generateScreenshots(
           viewport: targets[name].viewport,
           DomProvider,
         });
+
+        const errors = snapPayloads.filter((p) => p.isError);
+        if (errors.length === 1) {
+          throw errors[0];
+        }
+        if (errors.length > 1) {
+          throw new MultipleErrors(errors);
+        }
+
         const globalCSS = cssBlocks.concat([{ css }]);
 
         const { buffer, hash } = await prepareAssetsPackage({
@@ -231,9 +232,7 @@ async function generateScreenshots(
           }
         } else {
           if (VERBOSE === 'true') {
-            console.log(
-              `${logTag(project)}Uploading assets package ${hash}`,
-            );
+            console.log(`${logTag(project)}Uploading assets package ${hash}`);
           }
           assetsPackage = await uploadAssets({
             endpoint,