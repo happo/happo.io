87a7934a73eb3eeb4c610fd7c981ab9f0bdd8515
Henric Trotzig <henric.trotzig@gmail.com>
2021-12-15 14:37:20 +0100

    Talk to a "real" API endpoint in static-test.js
    Instead of mocking things, we can verify that the right things are sent
    in the request.

--- test/integrations/static-test.js
+++ test/integrations/static-test.js
@@ -1,24 +1,51 @@
 import { execSync } from 'child_process';
 import path from 'path';
+import http from 'http';
 
 import MockTarget from './MockTarget';
 import * as defaultConfig from '../../src/DEFAULTS';
-import makeRequest from '../../src/makeRequest';
 import runCommand from '../../src/commands/run';
 
-jest.mock('../../src/makeRequest');
+const ASSETS_URL_PATTERN = /\/api\/snap-requests\/assets\/(.+)/;
 
 let subject;
 let config;
 let sha;
+let httpServer;
 
-beforeEach(() => {
-  makeRequest.mockReset();
-  makeRequest.mockImplementation(() =>
-    Promise.resolve({ path: 'staticpkg/foobar.zip' }),
-  );
+beforeAll(async () => {
+  httpServer = http.createServer((req, res) => {
+    const match = req.url.match(ASSETS_URL_PATTERN);
+    if (match) {
+      const hash = match[1];
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          path: `staticpkg/${hash}.zip`,
+        }),
+      );
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          url: 'http://foobar.com',
+        }),
+      );
+    }
+  });
+  await new Promise((resolve) => httpServer.listen(8990, resolve));
+});
+
+afterAll(async () => {
+  await new Promise((resolve) => httpServer.close(resolve));
+});
+
+beforeEach(async () => {
   sha = 'foobar';
   config = Object.assign({}, defaultConfig, {
+    apiKey: 'foobar',
+    apiSecret: 'barfoo',
+    endpoint: 'http://localhost:8990',
     project: 'the project',
     targets: { chrome: new MockTarget() },
     generateStaticPackage: () => ({ path: path.join(__dirname, 'static-files') }),
@@ -26,23 +53,27 @@ beforeEach(() => {
   subject = () => runCommand(sha, config, {});
 });
 
-it('sends the project name in the request', async () => {
-  await subject();
-  expect(makeRequest.mock.calls[1][0].body.project).toEqual('the project');
-});
-
 it('produces the static package', async () => {
   await subject();
-  expect(makeRequest.mock.calls[0][0].formData.payload.value).toBeTruthy();
-  expect(config.targets.chrome.staticPackage).toEqual('staticpkg/foobar.zip');
+  expect(config.targets.chrome.staticPackage).toEqual(
+    'staticpkg/716966f385c6d10e661d41f2780eec7e.zip',
+  );
 });
 
 it('has a consistent hash', async () => {
   await subject();
-  expect(makeRequest.mock.calls[0][0].url).toEqual('https://happo.io/api/snap-requests/assets/716966f385c6d10e661d41f2780eec7e');
+  expect(config.targets.chrome.staticPackage).toEqual(
+    'staticpkg/716966f385c6d10e661d41f2780eec7e.zip',
+  );
+  delete config.targets.chrome.staticPackage;
   execSync(`touch ${path.join(__dirname, 'static-files', 'iframe.html')}`);
-  expect(makeRequest.mock.calls[0][0].url).toEqual('https://happo.io/api/snap-requests/assets/716966f385c6d10e661d41f2780eec7e');
   await subject();
-  expect(makeRequest.mock.calls[0][0].url).toEqual('https://happo.io/api/snap-requests/assets/716966f385c6d10e661d41f2780eec7e');
-
+  expect(config.targets.chrome.staticPackage).toEqual(
+    'staticpkg/716966f385c6d10e661d41f2780eec7e.zip',
+  );
+  delete config.targets.chrome.staticPackage;
+  await subject();
+  expect(config.targets.chrome.staticPackage).toEqual(
+    'staticpkg/716966f385c6d10e661d41f2780eec7e.zip',
+  );
 });
