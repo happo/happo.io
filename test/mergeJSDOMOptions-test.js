ff9920e31246932abccc5b1fdfe36208ed05ccfc
Henric Trotzig <henric.trotzig@gmail.com>
2021-12-23 07:31:48 +0100

    Assert custom jsdom functions called last
    To make it easier to override globals set in jsdom options functions set
    by default config, we need to make sure that custom functions are called
    last. This was already in place, I'm just adding an assertion to make
    things "official". :)

--- test/mergeJSDOMOptions-test.js
+++ test/mergeJSDOMOptions-test.js
@@ -5,14 +5,20 @@ const defaultObj = {};
 const customObj = {};
 
 const defaultOptions = {
-  modifyTestObj: obj => { obj.default = 'default'; },
+  modifyTestObj: (obj) => {
+    obj.default = 'default';
+    obj.winner = 'default';
+  },
   string: 'default-string',
   nonOverridenString: 'non overriden string',
   obj: defaultObj,
 };
 
 const customOptions = {
-  modifyTestObj: obj => { obj.custom = 'custom'; },
+  modifyTestObj: (obj) => {
+    obj.custom = 'custom';
+    obj.winner = 'custom';
+  },
   string: 'custom-string',
   obj: customObj,
   nullish: null,
@@ -27,6 +33,7 @@ it('correctly merges 2 jsdom options objects', () => {
   expect(merged.nonOverridenString).toEqual(defaultOptions.nonOverridenString);
   expect(testObj.default).toEqual('default');
   expect(testObj.custom).toEqual('custom');
+  expect(testObj.winner).toEqual('custom');
   expect(merged.obj).toEqual(customObj);
   expect(merged.nullish).toEqual(null);
 });
